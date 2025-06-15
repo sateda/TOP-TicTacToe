1aa8d0321012ad11a9f266435c6cbd6581d8d77a
sateda <7441698+sateda@users.noreply.github.com>
2025-06-15 20:52:40 +0200

    Javascript scaffolding

---
+++ script.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * Factory Player
+ * 
+ */
+
+function createPlayer(name) {
+    return { name };
+}
+
+const test = createPlayer("test");
+console.log(test);
+
+/**
+ * 
+ * Factory game controller
+ * 
+ */
+
+function gameController() {
+    
+
+}
+
+/**
+ * 
+ * Game board module
+ * 
+ */
+
+const gameboard = (function(){
+    // code goes here
+
+
+    // return functions
+    return {};
+})();
