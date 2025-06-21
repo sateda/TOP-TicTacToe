08d30e2cc3e0e6cf0cc66e9b19df0b72edab16ae
sateda <7441698+sateda@users.noreply.github.com>
2025-06-21 14:08:06 +0200

    add an check winner function

--- script.js
+++ script.js
@@ -6,8 +6,6 @@
 
 function createPlayer(name) {
     // set score function
-    
-    
     return { name };
 }
 
@@ -17,10 +15,46 @@ function createPlayer(name) {
  * 
  */
 
-function gameController() {
+const gameController = (function() {
     
+    function checkWinner() {
+        const board = gameBoard.getBoard();
 
-}
+        // check row on identical values
+        for(let i=0; i<3; i++) {
+            if(board[i][0] === "x" && board[i][1] === "x" && board[i][2] === "x") {
+                console.log("winner is player 1");
+            } else if (board[i][0] === "o" && board[i][1] === "o" && board[i][2] === "o") {
+                console.log("winner is player 2");
+            }
+        }
+
+        // check column on identical values
+        for(let i=0; i<3; i++) {
+            if(board[0][i] === "x" && board[1][i] === "x" && board[2][i] === "x") {
+                console.log("winner is player 1");
+            } else if (board[0][i] === "o" && board[1][i] === "o" && board[2][i] === "o") {
+                console.log("winner is player 2");
+            }
+        }
+
+        // check diagonal left to right
+        if(board[0][0] === "x" && board[1][1] === "x" && board[2][2] === "x") {
+            console.log("winner is player 1");
+        } else if (board[0][0] === "o" && board[1][1] === "o" && board[2][2] === "o") {
+            console.log("winner is player 2");
+        }
+
+        // check diagonal right to left
+        if(board[0][2] === "x" && board[1][1] === "x" && board[2][0] === "x") {
+            console.log("winner is player 1");
+        } else if (board[0][2] === "o" && board[1][1] === "o" && board[2][0] === "o") {
+            console.log("winner is player 2");
+        }
+    }
+
+    return {checkWinner};
+})();
 
 /**
  * 
@@ -40,8 +74,7 @@ const gameBoard = (function(){
     }
     
     function getBoard() {
-        console.log(board);
-        // return board;
+        return board;
     }
 
     function clearBoard() {
@@ -63,9 +96,11 @@ const gameBoard = (function(){
  * 
  */
 
-gameBoard.setBoardCell(2,2, "X");
-gameBoard.getBoard();
-gameBoard.setBoardCell(1,1,"O");
-gameBoard.clearBoard();
-gameBoard.getBoard();
+const player1 = createPlayer("hi");
+const player2 = createPlayer("oeps two");
+
+gameBoard.setBoardCell(0,2,"o");
+gameBoard.setBoardCell(1,1,"o");
+gameBoard.setBoardCell(2,0,"o");
+gameController.checkWinner();
 
