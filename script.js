b67bf9d9dd51755e06ba5c9b0b2cafab78da68d6
sateda <7441698+sateda@users.noreply.github.com>
2025-07-05 14:24:37 +0200

    Add check for a tie

--- script.js
+++ script.js
@@ -23,36 +23,45 @@ const gameController = (function() {
         // check row on identical values
         for(let i=0; i<3; i++) {
             if(board[i][0] === "x" && board[i][1] === "x" && board[i][2] === "x") {
-                console.log("winner is player 1");
+                return console.log("winner is player 1");
             } else if (board[i][0] === "o" && board[i][1] === "o" && board[i][2] === "o") {
-                console.log("winner is player 2");
+                return console.log("winner is player 2");
             }
         }
 
         // check column on identical values
         for(let i=0; i<3; i++) {
             if(board[0][i] === "x" && board[1][i] === "x" && board[2][i] === "x") {
-                console.log("winner is player 1");
+                return console.log("winner is player 1");
             } else if (board[0][i] === "o" && board[1][i] === "o" && board[2][i] === "o") {
-                console.log("winner is player 2");
+                return console.log("winner is player 2");
             }
         }
 
         // check diagonal left to right
         if(board[0][0] === "x" && board[1][1] === "x" && board[2][2] === "x") {
-            console.log("winner is player 1");
+            return console.log("winner is player 1");
         } else if (board[0][0] === "o" && board[1][1] === "o" && board[2][2] === "o") {
-            console.log("winner is player 2");
+            return console.log("winner is player 2");
         }
 
         // check diagonal right to left
         if(board[0][2] === "x" && board[1][1] === "x" && board[2][0] === "x") {
-            console.log("winner is player 1");
+            return console.log("winner is player 1");
         } else if (board[0][2] === "o" && board[1][1] === "o" && board[2][0] === "o") {
-            console.log("winner is player 2");
+            return console.log("winner is player 2");
         }
-    }
 
+        // check if it's a tie
+        for (let i=0; i<3; i++) {
+            for(let j=0; j<3; j++) {
+                if (board[i][j] = "") {
+                    console.log("next round");
+                }
+            }
+        }
+        return console.log("it's a tie!");
+    }
     return {checkWinner};
 })();
 
@@ -80,7 +89,8 @@ const gameBoard = (function(){
     function clearBoard() {
         for (let i=0; i<3; i++) {
             for(let j=0; j<3; j++) {
-                board[i][j] = "";
+                board[i][j] === "";
+                return console.log("not a tie!");
             }
         }
     }
@@ -99,8 +109,8 @@ const gameBoard = (function(){
 const player1 = createPlayer("hi");
 const player2 = createPlayer("oeps two");
 
-gameBoard.setBoardCell(0,2,"o");
-gameBoard.setBoardCell(1,1,"o");
-gameBoard.setBoardCell(2,0,"o");
+gameBoard.setBoardCell(0,0,"x");
+gameBoard.setBoardCell(0,1,"x");
+gameBoard.setBoardCell(0,2,"x");
 gameController.checkWinner();
 
